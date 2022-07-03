4008a5fcb703c144c921dd29eae187cacf34c9af
kashiwara0205 <tamatebako0205@gmail.com>
2022-07-03 11:04:55 +0900

    変数名を整えた

--- app/app/javascript/components/original/custom.tsx
+++ app/app/javascript/components/original/custom.tsx
@@ -7,55 +7,58 @@ const Input = styled('input')(({ theme }) => ({
   width: "auto",
   padding: 5,
   margin: 5,
+  border: "1px solid",
   backgroundColor: theme.palette.background.paper,
 }));
 
 const Listbox = styled('ul')(({ theme }) => ({
-    width: 200,
-    marginTop: 0,
-    marginBottom: 0,
-    marginRight: 0,
-    marginLeft: 5,
-    padding: 0,
-    zIndex: 1,
-    position: 'absolute',
-    listStyle: 'none',
-    backgroundColor: theme.palette.background.paper,
-    overflow: 'auto',
-    maxHeight: 200,
-    border: '1px solid rgba(0,0,0,.25)',
-    [`& li.${autocompleteClasses.focused}`]: {
-      backgroundColor: '#4a8df6',
-      color: 'white',
-      cursor: 'pointer',
-    },
-    '& li:active': {
-      backgroundColor: '#2977f5',
-      color: 'white',
-    },
+  marginTop: 0,
+  marginBottom: 0,
+  marginRight: 0,
+  marginLeft: 5,
+  padding: 0,
+  zIndex: 1,
+  position: 'absolute',
+  listStyle: 'none',
+  backgroundColor: theme.palette.background.paper,
+  overflow: 'auto',
+  maxHeight: 200,
+  border: '1px solid rgba(0,0,0,.25)',
+  [`& li.${autocompleteClasses.focused}`]: {
+    backgroundColor: '#4a8df6',
+    color: 'white',
+    cursor: 'pointer',
+  },
+  '& li:active': {
+    backgroundColor: '#2977f5',
+    color: 'white',
+  },
+}));
+
+const Button = styled('button')(({ theme }) => ({
+  cursor: "pointer", 
+  marginLeft: 0, 
+  padding: 5
 }));
 
 type Props = {
-    options
-    value
-    width
+  options
+  value
+  width
 }
 export default function UseAutocomplete(props: Props) {
-
-    const ref1 = useRef(null)
+  const deleteButtonRef = useRef(null)
 
   const [inputValue, setInputValue] = useState(props.value);
-  const [exp, setExp] = useState("");
-  const [filterdGroupOptions, setFilterdGroupOptions] = useState([]);
-  const [testState, setTestState] = useState(true);
+  const [filterExp, setFilterExp] = useState("");
+  const [avaiableListBox, setAvaiableListBox] = useState(true);
 
-  const isOptionEqualToValue = (va1, va2) =>{
-    return true
-  }
+  const isOptionEqualToValue = (va1, va2) =>{ return true }
+
+  const getOptionLabel = (option: string) =>{ return option}
 
   const {
     getRootProps,
-    getInputLabelProps,
     getInputProps,
     getListboxProps,
     getOptionProps,
@@ -64,80 +67,76 @@ export default function UseAutocomplete(props: Props) {
     id: 'use-autocomplete-demo',
     value: inputValue,
     options: props.options,
-    getOptionLabel: (option: string) => option,
+    getOptionLabel: getOptionLabel,
     isOptionEqualToValue:isOptionEqualToValue
   });
 
-  const groupedOptions2 = groupedOptions
-
   const onCLickLi = (option)=>{
-    console.log("onCLickLi")
     setInputValue(option)
-    setTestState(false)
+    setAvaiableListBox(false)
   }
 
   const onClickDelete = ()=>{
     setInputValue("")
-    ref1.current.focus()
-    ref1.current.blur()
-    setTestState(false)
+    deleteButtonRef.current.focus()
+    deleteButtonRef.current.blur()
+    setAvaiableListBox(false)
   }
 
-  const onChange = (e) =>{
-    setTestState(true)
+  const onChangeInput = (e) =>{
+    setAvaiableListBox(true)
     setInputValue(e.target.value)
-    setExp(".*" + e.target.value + ".*")
+    setFilterExp(".*" + e.target.value + ".*")
   }
 
   const onClickInput = (e) =>{
-    setTestState(true)
+    setAvaiableListBox(true)
     console.log("onClickInput")
     setInputValue(e.target.value)
-    setExp("")
+    setFilterExp("")
+  }
+
+  const onKeyDownCapture = (e) =>{
+    e.stopPropagation();
   }
 
   const fileter = (options) =>{
-    const expObj = new RegExp(exp);
+    const expObj = new RegExp(filterExp);
     const newOptions = options.filter((value)=> { return value.match(expObj) })
     return newOptions
   }
 
   const RenderList = () =>{
-
-      let fileterd = []
-      if(testState){
-        fileterd = fileter(groupedOptions2)
-      }
-
-      console.log(testState)
-      console.log(fileterd)
-      if (fileterd.length > 0 ){
-        return  (
-            <div id="inputmemo" >
-          <Listbox  {...getListboxProps()}>
-            {(fileterd as typeof props.options).map((option, index) => (
-              <li {...getOptionProps({ option, index })} onClick={(e)=>{onCLickLi(option)}} >&nbsp;{option}</li>
-            ))}
-          </Listbox>
-          </div>
-            )
-      }else{
-          return (
-          <div></div>
-          )
-      }
+    let fileterdGroupOptions = []
+    if(avaiableListBox){
+      fileterdGroupOptions = fileter(groupedOptions)
     }
 
+    if (fileterdGroupOptions.length > 0 ){
+      return  (
+        <div>
+          <Listbox  {...getListboxProps()} style={{"width": props.width}}>
+            {(fileterdGroupOptions as typeof props.options).map((option, index) => (
+              <li {...getOptionProps({ option, index })} onClick={()=>{onCLickLi(option)}} >&nbsp;{option}</li>
+            ))}
+          </Listbox>
+        </div>
+      )
+    }else{
+      return (
+        null
+      )
+    }
+  }
+  
   return (
-    <div onKeyDownCapture={(e) => {
-          e.stopPropagation();
-      }}>
+    <div onKeyDownCapture={onKeyDownCapture}>
       <div {...getRootProps()}>
-        <Input {...getInputProps()} style={{ width: props.width, "border": "1px solid"}} onClick={(e) =>{onClickInput(e)}} value={inputValue} onChange={(e)=>{onChange(e)}}/>
-        <button ref={ref1}style={{  "cursor": "pointer", "marginLeft": 0, "padding": 5}} className="delete" onClick={onClickDelete}> x </button>
+        <Input {...getInputProps()} style={{ width: props.width}} onClick={onClickInput} value={inputValue} onChange={onChangeInput}/>
+        <Button ref={deleteButtonRef} onClick={onClickDelete}> x </Button>
       </div>
       <div >
-      <RenderList  />
+        <RenderList  />
       </div>
     </div>
   );
